608909745f4db6901095f9f54e48a9c13a4e9d62
rishuc07 <rishabhsinghaug@gmail.com>
2023-09-29 09:27:01 +0530

    AWS Elastic Beanstalk

--- application.py
+++ application.py
@@ -8,14 +8,14 @@ from src.pipeline.predict_pipeline import CustomData,PredictPipeline
 
 application = Flask(__name__)
 
-
+app = application
 
 ## Route to a home page
-@application.route('/')
+@app.route('/')
 def index():
     return render_template("index.html")
 
-@application.route("/predictdata",methods=['GET','POST'])
+@app.route("/predictdata",methods=['GET','POST'])
 def predict_datapoint():
     if request.method=='GET':
         return render_template('home.html')
@@ -42,4 +42,4 @@ def predict_datapoint():
         return render_template('home.html',results=results[0])
     
 if __name__ =='__main__':
-    application.run(host='0.0.0.0')
+    app.run(host='0.0.0.0')
